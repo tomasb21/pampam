1b12dbb9c084c03a4d5204177335fc66d4e928ba
tomasb21 <tomasb330@gmail.com>
2025-06-09 00:23:55 +0100

    Update binomial_two_class.hpp
    mp

--- src/glmnetpp/include/glmnetpp_bits/elnet_path/binomial_two_class.hpp
+++ src/glmnetpp/include/glmnetpp_bits/elnet_path/binomial_two_class.hpp
@@ -34,6 +34,7 @@ public:
     template <class ValueType
             , class JUType
             , class VPType
+            , class MPType
             , class CLType
             , class IntType
             , class XType
@@ -53,6 +54,7 @@ public:
         ValueType beta,
         const JUType& ju,
         const VPType& vp,
+        const MPType& mp,
         const CLType& cl,
         IntType ne,
         IntType nx,
@@ -85,6 +87,7 @@ public:
             ValueType
             , JUType
             , VPType
+            , MPType
             , CLType
             , IntType
             , XType
@@ -101,7 +104,7 @@ public:
             , SetpbFType
             , IntParamType> pack{
                 // build sub-pack
-                {beta, ju, vp, cl, ne, nx, x, nlam, flmin,
+                {beta, ju, vp, mp, cl, ne, nx, x, nlam, flmin,
                  ulam, thr, maxit, lmu, ao, ia, kin, alm, nlp, jerr, setpb_f, int_param},
                 // add new members
                 y, g, w, isd, intr, kopt, a0,  dev0, dev
@@ -118,7 +121,7 @@ public:
         return elnet_point_t(
                 pack.isd, pack.intr, pack.kopt, sp.thr, sp.maxit,
                 sp.nx, sp.nlp, sp.ia, pack.g, pack.dev0, 
-                sp.x, pack.y, pack.w, sp.vp, sp.cl, sp.ju, 
+                sp.x, pack.y, pack.w, sp.vp, sp.mp, sp.cl, sp.ju, 
                 sp.int_param);
     }
 };
