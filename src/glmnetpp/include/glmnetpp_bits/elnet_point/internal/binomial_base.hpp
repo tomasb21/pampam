11a80ae063fb1631a9ee27baf6ceac22b83657c7
tomasb21 <tomasb330@gmail.com>
2025-06-26 12:13:31 +0100

    Update binomial_base.hpp
    + penalty_matrix

--- src/glmnetpp/include/glmnetpp_bits/elnet_point/internal/binomial_base.hpp
+++ src/glmnetpp/include/glmnetpp_bits/elnet_point/internal/binomial_base.hpp
@@ -559,7 +559,7 @@ protected:
     void initialize(const PackType& p) 
     { 
         base_t::compute_strong_map(
-                this->abs_grad(), this->penalty(), this->penalty(), this->strong_map(),
+                this->abs_grad(), this->penalty_matrix(), this->penalty_matrix(), this->strong_map(),
                 p.elastic_prop(), p.lmda(), p.prev_lmda(), 
                 [&](auto k) { return !this->is_excluded(k) || !this->exclusion()[k]; });
     }
